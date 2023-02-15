e489dec8e0301e550be4bec876baff11f4bfb5d3
JuanMaII <juanmasotonp@gmail.com>
2023-02-15 19:54:07 -0300

    agregamos path al routing

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Pagina404Component } from './component/pagina404/pagina404.component';
 
 const routes: Routes = [
-  {}
+  {path: '**', component: Pagina404Component},
+  {path: '', redirectTo:'/inicio', pathMatch:'full'}
 ];
 
 @NgModule({
