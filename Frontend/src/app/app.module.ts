fd09b0106ef588c96e8352dfa303042c070deba2
JuanMaII <juanmasotonp@gmail.com>
2023-02-18 21:37:00 -0300

    agregamos login authentic service

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pagina404Component } from './component/pagina404/pagina404.component';
+import { LogInComponent } from './logIn/log-in/log-in.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Pagina404Component
+    Pagina404Component,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
