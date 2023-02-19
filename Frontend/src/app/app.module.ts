4c5b6cee4aa394867e5de36eb5830dd48d9d1399
JuanMaII <juanmasotonp@gmail.com>
2023-02-18 23:22:09 -0300

    agregamos protectores de ruta

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Pagina404Component } from './component/pagina404/pagina404.component';
 import { LogInComponent } from './logIn/log-in/log-in.component';
+import { HomeComponentComponent } from './Home/home-component/home-component.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Pagina404Component,
-    LogInComponent
+    LogInComponent,
+    HomeComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- Frontend/src/app/logIn/log-in/log-in.component.ts
+++ Frontend/src/app/logIn/log-in/log-in.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AuthService } from 'src/app/Auth/auth.service';
+
 
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
   styleUrls: ['./log-in.component.sass']
 })
-export class LogInComponent {
+export class LogInComponent implements OnInit {
 
-}
+  email = '';
+  password = '';
+  
+  constructor(private authService: AuthService){}
+  
+  Login (){
+    this.authService.login(this.email, this.password)
+   }
+
+   ngOnInit(): void {
+     
+   }
+  }
+
+  
+
